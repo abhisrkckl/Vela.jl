deddde47c493f3b68f0a437ff86e19a72fbc4c6a
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-01-04 16:57:22 -0600

    toa

---
+++ src/toa.jl
@@ -0,0 +1,15 @@
+using GeometricUnits
+using Quadmath
+
+struct TOA
+    val::GQ{Float128}
+    err::GQ{Float64}
+    freq::GQ{Float64}
+    clkcorr::GQ{Float64}
+    deltapn::GQ{Float64}
+    ssb_obs_pos::Tuple{GQ{Float64}, GQ{Float64}, GQ{Float64}}
+    ssb_obs_vel::Tuple{GQ{Float64}, GQ{Float64}, GQ{Float64}}
+    obs_sun_pos::Tuple{GQ{Float64}, GQ{Float64}, GQ{Float64}}
+    level::UInt
+    phase::GQ{Float128}
+end
