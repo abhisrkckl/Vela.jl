9a9e3303d2c76b8ecb81258d90bd468e159b28be
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-01-04 21:03:47 -0600

    toa

--- src/toa.jl
+++ src/toa.jl
@@ -1,17 +1,27 @@
 using GeometricUnits
 using Quadmath
 
-export TOA
+export TOA, is_barycentered, correct_toa_delay, correct_toa_phase
+
+struct EphemerisVectors
+    ssb_obs_pos::Vector{GQ{Float64}}
+    ssb_obs_vel::Vector{GQ{Float64}}
+    obs_sun_pos::Vector{GQ{Float64}}
+end
 
 struct TOA
-    val::GQ{Float128}
-    err::GQ{Float64}
-    freq::GQ{Float64}
-    clkcorr::GQ{Float64}
-    deltapn::GQ{Float64}
-    ssb_obs_pos::Tuple{GQ{Float64}, GQ{Float64}, GQ{Float64}}
-    ssb_obs_vel::Tuple{GQ{Float64}, GQ{Float64}, GQ{Float64}}
-    obs_sun_pos::Tuple{GQ{Float64}, GQ{Float64}, GQ{Float64}}
-    level::UInt
+    value::GQ{Float128}
+    error::GQ{Float64}
+    frequency::GQ{Float64}
     phase::GQ{Float128}
+    ephem_vectors::Union{EphemerisVectors,Nothing}
+    level::UInt
 end
+
+TOA(value, error, frequency, phase, ephem_vectors) = TOA(value, error, frequency, phase, ephem_vectors, 0)
+TOA(value, error, frequency, phase) = TOA(value, error, frequency, phase, nothing, 0)
+
+is_barycentered(toa::TOA) = isnothing(toa.ephem_vectors)
+
+correct_toa_delay(toa::TOA, delay::GQ) = TOA(toa.value - delay, toa.error, toa.frequency, toa.phase, toa.ephem_vectors, toa.level+1)
+correct_toa_phase(toa::TOA, phase::GQ) = TOA(toa.value, toa.error, toa.frequency, toa.phase + phase, toa.ephem_vectors, toa.level+1)
