757b6c2c900cf46dc5210b942b9312dcdf92b3bc
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-03-01 21:26:59 +0530

    fmt

--- src/toa.jl
+++ src/toa.jl
@@ -98,6 +98,5 @@ show(io::IO, toa::TOA) = print(
     "$(toa.tzr ? "TZR" : "")TOA[MJD:$(trunc(Int, toa.value.x/day_to_s)), Freq(MHz):$(trunc(Int, toa.observing_frequency.x/1e6))]",
 )
 show(io::IO, ::MIME"text/plain", toa::TOA) = show(io, toa)
-
 show(io::IO, toas::Vector{TOA}) = print(io, "[Vector of $(length(toas)) TOAs.]")
 show(io::IO, ::MIME"text/plain", toas::Vector{TOA}) = show(io, toas)
