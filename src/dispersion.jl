b045dd4fd0d71940a84106f623302e017b09488f
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-06-06 10:27:35 +0200

    docs

--- src/dispersion.jl
+++ src/dispersion.jl
@@ -1,10 +1,12 @@
 export DispersionTaylor, dispersion_slope
 
+"""Taylor series representation of the dispersion measure."""
 struct DispersionTaylor <: DispersionComponent end
 
 read_params_from_dict(::DispersionTaylor, params::Dict) =
     (DMEPOCH = params[:DMEPOCH][1], DM = params[:DM])
 
+"""Compute the dispersion slope corresponding to a TOA."""
 function dispersion_slope(::DispersionTaylor, toa::TOA, params)
     t0 = params.DMEPOCH
     t = toa.value
--- src/phase_offset.jl
+++ src/phase_offset.jl
@@ -2,6 +2,7 @@ using GeometricUnits
 
 export PhaseOffset, phase
 
+"""Phase offset between measured TOAs and the TZR TOA."""
 struct PhaseOffset <: PhaseComponent end
 
 read_params_from_dict(::PhaseOffset, params::Dict) = (PHOFF = params[:PHOFF][1],)
