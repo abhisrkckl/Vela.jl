292a31e88c329c52f5152bde6040bee02a1ad91d
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-06-07 11:48:22 +0200

    CorrectedTOA

--- src/toa.jl
+++ src/toa.jl
@@ -3,112 +3,149 @@ using Quadmath
 using LinearAlgebra
 import Base.copy, Base.show
 
-export TOA, correct_toa_delay, correct_toa_phase, make_tzr_toa, topocentric_frequency
+export TOA,
+    make_tzr_toa,
+    CorrectedTOA,
+    scaled_toa_error_sqr,
+    doppler_shifted_spin_frequency,
+    doppler_corrected_observing_frequency,
+    corrected_toa_value,
+    corrected_toa_phase,
+    correct_toa
 
-"""
-    A type representing a single narrow-band TOA.
-"""
+"""A single narrowband TOA observation."""
 struct TOA
     value::GQ{Float128}
     error::GQ{Float64}
     observing_frequency::GQ{Float64}
-    phase::GQ{Float128}
-    spin_frequency::GQ{Float64}
-    doppler::GQ{Float64}
+    delta_phase::GQ{Float64}
     barycentered::Bool
     tzr::Bool
-    level::UInt
     ephem::SolarSystemEphemeris
 
-    function TOA(
-        value,
-        error,
-        observing_frequency,
-        phase,
-        spin_frequency,
-        doppler,
-        barycentered,
-        tzr,
-        level,
-        ephem,
-    )
+    function TOA(value, error, observing_frequency, delta_phase, barycentered, tzr, ephem)
         @assert value.d == 1 "Dimension mismatch in value (given $(value.d), expected 1)."
         @assert error.d == 1 "Dimension mismatch in error (given $(error.d), expected 1)."
         @assert observing_frequency.d == -1 "Dimension mismatch in observing_frequency (given $(observing_frequency.d), expected -1)."
-        @assert spin_frequency.d == -1 "Dimension mismatch in spin_frequency (given $(spin_frequency.d), expected -1)."
+        @assert delta_phase.d == 0 "Dimension mismatch in delta_phase (given $(delta_phase.d), expected 0)."
+
+        return new(value, error, observing_frequency, delta_phase, barycentered, tzr, ephem)
+    end
+end
+
+TOA(value, error, observing_frequency, delta_phase, barycentered, ephem) =
+    TOA(value, error, observing_frequency, delta_phase, barycentered, false, ephem)
+
+"""Create a TZR TOA object."""
+make_tzr_toa(tzrtdb, tzrfreq, tzrbary, tzrephem) =
+    TOA(tzrtdb, time(0.0), tzrfreq, dimensionless(0.0), tzrbary, true, tzrephem)
+
+"""The accumulated timing & noise model corrections applied to a narrowband TOA."""
+struct CorrectedTOA
+    toa::TOA
+    delay::GQ{Float64}
+    phase::GQ{Float128}
+    efac::GQ{Float64}
+    equad2::GQ{Float64}
+    spin_frequency::GQ{Float64}
+    doppler::GQ{Float64}
+    barycentered::Bool
+    level::UInt
+
+    function CorrectedTOA(
+        toa,
+        delay,
+        phase,
+        efac,
+        equad2,
+        spin_frequency,
+        doppler,
+        barycentered,
+        level,
+    )
+        @assert delay.d == 1 "Dimension mismatch in value (given $(delay.d), expected 1)."
         @assert phase.d == 0 "Dimension mismatch in phase (given $(phase.d), expected 0)."
+        @assert efac.d == 0 "Dimension mismatch in efac (given $(efac.d), expected 0)."
+        @assert equad2.d == 2 "Dimension mismatch in equad (given $(equad2.d), expected 2)."
+        @assert spin_frequency.d == -1 "Dimension mismatch in spin_frequency (given $(spin_frequency.d), expected -1)."
+        @assert doppler.d == 0 "Dimension mismatch in doppler (given $(doppler.d), expected 0)."
+
+        @assert abs(doppler) < dimensionless(1.0) "|doppler| must be less than 1."
+        @assert spin_frequency == frequency(-1.0) || spin_frequency > frequency(0.0) "spin_frequency must either be a positive value or a default value of -1."
 
         return new(
-            value,
-            error,
-            observing_frequency,
+            toa,
+            delay,
             phase,
+            efac,
+            equad2,
             spin_frequency,
             doppler,
             barycentered,
-            tzr,
             level,
-            ephem,
         )
     end
 end
 
-TOA(value, error, observing_frequency, phase, barycentered, ephem) = TOA(
-    value,
-    error,
-    observing_frequency,
-    phase,
-    frequency(-1.0),
-    dimensionless(0.0),
-    barycentered,
-    false,
-    0,
-    ephem,
-)
-
-"""Create a TZR TOA object."""
-make_tzr_toa(tzrtdb, tzrfreq, tzrbary, tzrephem) = TOA(
-    tzrtdb,
+CorrectedTOA(toa) = CorrectedTOA(
+    toa,
     time(0.0),
-    tzrfreq,
     dimensionless(Float128(0.0)),
+    dimensionless(1.0),
+    GQ(0.0, 2),
     frequency(-1.0),
     dimensionless(0.0),
-    tzrbary,
-    true,
+    toa.barycentered,
     0,
-    tzrephem,
 )
 
-"""Correct the TOA object by a given time delay."""
-correct_toa_delay(toa::TOA, delay::GQ) = TOA(
-    toa.value - delay,
-    toa.error,
-    toa.observing_frequency,
-    toa.phase,
-    toa.spin_frequency,
-    toa.doppler,
-    toa.barycentered,
-    toa.tzr,
-    toa.level + 1,
-    toa.ephem,
-)
+"""Squared TOA uncertainty after applying EFAC and EQUAD."""
+scaled_toa_error_sqr(ctoa::CorrectedTOA) =
+    (ctoa.toa.error * ctoa.toa.error + ctoa.equad2) * ctoa.efac * ctoa.efac
 
-"""Correct a TOA object by a given phase."""
-correct_toa_phase(toa::TOA, phase::GQ) = TOA(
-    toa.value,
-    toa.error,
-    toa.observing_frequency,
-    toa.phase + phase,
-    toa.spin_frequency,
-    toa.doppler,
-    toa.barycentered,
-    toa.tzr,
-    toa.level + 1,
-    toa.ephem,
-)
+"""Spin frequency in topocentric or barycentric frame, depending on the correction level.
+The spin_frequency is originally in the pulsar frame."""
+function doppler_shifted_spin_frequency(ctoa::CorrectedTOA)::GQ
+    @assert ctoa.spin_frequency != frequency(-1.0) "The spin_frequency has not been set."
+    return ctoa.spin_frequency * (1 + ctoa.doppler)
+end
 
-topocentric_frequency(toa::TOA)::GQ = toa.spin_frequency * (1 - toa.doppler)
+"""Observing frequency in the barycentric or pulsar frame, depending on the correction level.
+The observing_frequency is originally in the topocentric frame."""
+doppler_corrected_observing_frequency(ctoa::CorrectedTOA)::GQ =
+    ctoa.toa.observing_frequency * (1 - ctoa.doppler)
+
+"""TOA value after delay correction with 128-bit precision."""
+corrected_toa_value_F128(ctoa::CorrectedTOA) = ctoa.toa.value - ctoa.delay
+
+"""TOA value after delay correction with 64-bit precision."""
+corrected_toa_value(ctoa::CorrectedTOA) = GQ{Float64}(ctoa.toa.value) - ctoa.delay
+
+"""TOA phase including delta_phase."""
+corrected_toa_phase(ctoa::CorrectedTOA) = ctoa.toa.delta_phase + ctoa.phase
+
+"""Apply a correction to a CorrectedTOA object.
+"""
+correct_toa(
+    ctoa::CorrectedTOA;
+    delay::GQ = time(0.0),
+    phase::GQ = dimensionless(0.0),
+    efac::GQ = dimensionless(1.0),
+    equad2::GQ = GQ(0.0, 2),
+    delta_spin_frequency::GQ = frequency(0.0),
+    doppler::GQ = dimensionless(0.0),
+    barycentered = false,
+) = CorrectedTOA(
+    ctoa.toa,
+    ctoa.delay + delay,
+    ctoa.phase + phase,
+    ctoa.efac * efac,
+    ctoa.equad2 + equad2,
+    ctoa.spin_frequency + delta_spin_frequency,
+    ctoa.doppler + doppler,
+    ctoa.barycentered || barycentered,
+    ctoa.level + 1,
+)
 
 const day_to_s = 86400
 show(io::IO, toa::TOA) = print(
