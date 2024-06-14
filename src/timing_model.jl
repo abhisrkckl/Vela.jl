2c386476a7fbc14552ddb87a405c2ee2563d452a
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-06-14 22:17:02 +0200

    form_residuals

--- src/timing_model.jl
+++ src/timing_model.jl
@@ -65,12 +65,13 @@ function form_residual(model::TimingModel, toa::TOA, params::NamedTuple, tzrphas
     return dphase / doppler_shifted_spin_frequency(ctoa)
 end
 
-function form_residuals(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)::Vector{GQ}
+function form_residuals(
+    model::TimingModel,
+    toas::Vector{TOA},
+    params::NamedTuple,
+)::Vector{GQ}
     tzrphase = calc_tzr_phase(model, params)
-    return [
-        form_residual(model, toa, params, tzrphase)
-        for toa in toas
-    ]
+    return [form_residual(model, toa, params, tzrphase) for toa in toas]
 end
 
 function calc_tzr_phase(model::TimingModel, params::NamedTuple)
