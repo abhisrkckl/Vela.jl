9b4cc6893a347885ba73d9cab96cf5b9daa7721e
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-05 15:59:49 +0200

    residuals

--- src/timing_model.jl
+++ src/timing_model.jl
@@ -54,26 +54,6 @@ correct_toa(model::TimingModel, ctoa::CorrectedTOA, params::NamedTuple) =
 correct_toa(model::TimingModel, toa::TOA, params::NamedTuple) =
     correct_toa(model, CorrectedTOA(toa), params)
 
-function form_residual(model::TimingModel, toa::TOA, params::NamedTuple, tzrphase::GQ)::GQ
-    ctoa = correct_toa(model, toa, params)
-    dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
-    return dphase / doppler_shifted_spin_frequency(ctoa)
-end
-
-function form_residuals(
-    model::TimingModel,
-    toas::Vector{TOA},
-    params::NamedTuple,
-)::Vector{GQ}
-    tzrphase = calc_tzr_phase(model, params)
-    return [form_residual(model, toa, params, tzrphase) for toa in toas]
-end
-
-function calc_tzr_phase(model::TimingModel, params::NamedTuple)
-    ctzrtoa = correct_toa(model, model.tzr_toa, params)
-    return ctzrtoa.phase
-end
-
 function calc_chi2(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)
     tzrphase = calc_tzr_phase(model, params)
 
