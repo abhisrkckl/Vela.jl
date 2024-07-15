79026f93b2daa9036df397b7fe9bbce5a5e2a939
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-15 11:08:06 +0200

    no explicit gil release

--- src/pyinter.jl
+++ src/pyinter.jl
@@ -10,21 +10,8 @@ get_chi2_serial_func(model::TimingModel, toas::Vector{TOA}) =
 calc_chi2(model::TimingModel, toas::Vector{TOA}, params::PyArray) =
     calc_chi2(model, toas, Vector{Float64}(params))
 
-function get_chi2_parallel_func(model::TimingModel, toas::Vector{TOA})
-    return params -> begin
-        result = -Inf
-
-        # Release the GIL for parallel computation
-        pythread = PyEval_SaveThread()
-        try
-            result = calc_chi2(model, toas, params)
-        finally
-            PyEval_RestoreThread(pythread)
-        end
-
-        return result
-    end
-end
+get_chi2_parallel_func(model::TimingModel, toas::Vector{TOA}) =
+    params -> calc_chi2(model, toas, params)
 
 calc_lnlike_serial(model::TimingModel, toas::Vector{TOA}, params::PyArray) =
     calc_lnlike_serial(model, toas, Vector{Float64}(params))
@@ -35,18 +22,4 @@ get_lnlike_serial_func(model::TimingModel, toas::Vector{TOA}) =
 calc_lnlike(model::TimingModel, toas::Vector{TOA}, params::PyArray) =
     calc_lnlike(model, toas, Vector{Float64}(params))
 
-function get_lnlike_parallel_func(model, toas)
-    return params -> begin
-        result = -Inf
-
-        # Release the GIL for parallel computation
-        pythread = PyEval_SaveThread()
-        try
-            result = calc_lnlike(model, toas, params)
-        finally
-            PyEval_RestoreThread(pythread)
-        end
-
-        return result
-    end
-end
+get_lnlike_parallel_func(model, toas) = params -> calc_lnlike(model, toas, params)
