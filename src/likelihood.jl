6ea13f5aa2e20c01a8eba44c5ef6bcf0c049ecc2
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-22 10:12:17 +0200

    term funcs

--- src/likelihood.jl
+++ src/likelihood.jl
@@ -1,15 +1,18 @@
+function _chi2_term(model::TimingModel, toa::TOA, params::NamedTuple, tzrphase)
+    ctoa = correct_toa(model, toa, params)
+    dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
+    tres = dphase / doppler_shifted_spin_frequency(ctoa)
+    err2 = scaled_toa_error_sqr(ctoa)
+    return value(tres * tres / err2)
+end
+
 """Compute the χ^2 value for a given timing model and collection of TOAs (parallel execution)."""
 function calc_chi2(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)
     tzrphase = calc_tzr_phase(model, params)
 
     chisq = Atomic{Float64}(0.0)
     @threads for toa in toas
-        ctoa = correct_toa(model, toa, params)
-        dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
-        tres = dphase / doppler_shifted_spin_frequency(ctoa)
-        err2 = scaled_toa_error_sqr(ctoa)
-
-        atomic_add!(chisq, Float64((tres * tres / err2).x))
+        atomic_add!(chisq, _chi2_term(model, toa, params, tzrphase))
     end
 
     return chisq[]
@@ -21,22 +24,20 @@ calc_chi2(model::TimingModel, toas::Vector{TOA}, params) =
 """Compute the χ^2 value for a given timing model and collection of TOAs (serial execution)."""
 function calc_chi2_serial(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)
     tzrphase = calc_tzr_phase(model, params)
-
-    chisq = 0.0
-    for toa in toas
-        ctoa = correct_toa(model, toa, params)
-        dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
-        tres = dphase / doppler_shifted_spin_frequency(ctoa)
-        err2 = scaled_toa_error_sqr(ctoa)
-
-        chisq += (tres * tres / err2).x
-    end
-
-    return chisq
+    return sum(toa -> _chi2_term(model, toa, params, tzrphase), toas)
 end
 
 calc_chi2_serial(model::TimingModel, toas::Vector{TOA}, params) =
     calc_chi2_serial(model, toas, read_params(model, params))
+
+function _lnlike_term(model::TimingModel, toa::TOA, params::NamedTuple, tzrphase)
+    ctoa = correct_toa(model, toa, params)
+    dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
+    tres = dphase / doppler_shifted_spin_frequency(ctoa)
+    err2 = scaled_toa_error_sqr(ctoa)
+    norm = log(value(err2))
+    return value(tres * tres / err2) + norm
+end
 
 """Compute the log-likelihood value for a given timing model and collection of TOAs (parallel execution)."""
 function calc_lnlike(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)
@@ -44,13 +45,7 @@ function calc_lnlike(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)
 
     result = Atomic{Float64}(0.0)
     @threads for toa in toas
-        ctoa = correct_toa(model, toa, params)
-        dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
-        tres = dphase / doppler_shifted_spin_frequency(ctoa)
-        err2 = scaled_toa_error_sqr(ctoa)
-        norm = log(err2.x)
-
-        atomic_add!(result, (tres * tres / err2).x + norm)
+        atomic_add!(result, _lnlike_term(model, toa, params, tzrphase))
     end
 
     return -result[] / 2
@@ -62,19 +57,7 @@ calc_lnlike(model::TimingModel, toas::Vector{TOA}, params) =
 """Compute the log-likelihood value for a given timing model and collection of TOAs (serial execution)."""
 function calc_lnlike_serial(model::TimingModel, toas::Vector{TOA}, params::NamedTuple)
     tzrphase = calc_tzr_phase(model, params)
-
-    result = 0.0
-    for toa in toas
-        ctoa = correct_toa(model, toa, params)
-        dphase = GQ{Float64}(phase_residual(ctoa) - tzrphase)
-        tres = dphase / doppler_shifted_spin_frequency(ctoa)
-        err2 = scaled_toa_error_sqr(ctoa)
-        norm = log(err2.x)
-
-        result += (tres * tres / err2).x + norm
-    end
-
-    return -result / 2
+    return -sum(toa -> _lnlike_term(model, toa, params, tzrphase), toas) / 2
 end
 
 calc_lnlike_serial(model::TimingModel, toas::Vector{TOA}, params) =
