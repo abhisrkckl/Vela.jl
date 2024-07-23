ae40be19484f7c4511b5daa55e6de6e66bc17e10
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-23 15:46:17 +0200

    faster jump

--- src/jump.jl
+++ src/jump.jl
@@ -2,24 +2,22 @@ export PhaseJump
 
 """System-dependent phase jumps."""
 struct PhaseJump <: PhaseComponent
-    jump_mask::BitMatrix
+    jump_mask::Matrix{Float64}
 end
 
 function phase(pjmp::PhaseJump, ctoa::CorrectedTOA, params::NamedTuple)::GQ
-    @assert length(params.JUMP) == pjmp.jump_mask.dims[1]
-    @assert ctoa.toa.index <= pjmp.jump_mask.dims[2]
+    @assert length(params.JUMP) == size(pjmp.jump_mask)[1]
+    @assert ctoa.toa.index <= size(pjmp.jump_mask)[2]
     if ctoa.toa.tzr
         return dimensionless(0.0)
     else
-        return sum(
-            jj -> pjmp.jump_mask[jj, ctoa.toa.index] * params.JUMP[jj],
-            1:length(params.JUMP),
-        ) * (params.F_ + params.F[1])
+        return dot(params.JUMP, @view(pjmp.jump_mask[:, ctoa.toa.index])) *
+               (params.F_ + params.F[1])
     end
 end
 
 function show(io::IO, jmp::PhaseJump)
-    num_jumps = jmp.jump_mask.dims[1]
+    num_jumps = size(jmp.jump_mask)[1]
     print(io, "PhaseJump($num_jumps JUMPs)")
 end
 show(io::IO, ::MIME"text/plain", jmp::PhaseJump) = show(io, jmp)
