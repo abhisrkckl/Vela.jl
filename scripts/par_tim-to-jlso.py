13847c956f5a36fc162e626eeed4f41189d2481b
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-24 15:32:42 +0200

    par_tim_to_jlso

--- scripts/par_tim-to-jlso.py
+++ scripts/par_tim-to-jlso.py
@@ -1,25 +1,8 @@
 #!/usr/bin/env python
 
-from pint2vela import read_model_and_toas, vl
-from juliacall import Main as jl
-
+from pint2vela import par_tim_to_jlso
 import sys
-
-jl.seval("import JLSO")
 
 parfile, timfile, jlsofile = sys.argv[1], sys.argv[2], sys.argv[3]
 
-mv, tv = read_model_and_toas(parfile, timfile)
-
-jl.JLSO.save(
-    jlsofile, 
-    jl.Pair(
-        jl.Symbol("model"),
-        mv,
-    ),
-    jl.Pair(
-        jl.Symbol("toas"),
-        tv,
-    )
-)
-
+par_tim_to_jlso(parfile, timfile, jlsofile)
---
+++ tests/test_convert_jlso.py
@@ -0,0 +1,10 @@
+from pint2vela import par_tim_to_jlso, vl
+import os
+
+def test_convert_jlso():
+    par_tim_to_jlso("datafiles/NGC6440E.par", "datafiles/NGC6440E.tim", "__NGC6440E.jlso")
+    assert os.path.isfile("__NGC6440E.jlso")
+
+    mv, tv = vl.load_pulsar_data("__NGC6440E.jlso")
+    assert len(tv) == 62
+    assert set(vl.get_free_param_names(mv.param_handler)) == {"RAJ", "DECJ", "DM", "PHOFF", "F0", "F1"}
