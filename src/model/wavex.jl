4596df73f3a884db0ddc2670477f172a139c5718
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-25 15:26:31 +0200

    --

--- src/model/wavex.jl
+++ src/model/wavex.jl
@@ -2,13 +2,14 @@ export WaveX, DMWaveX
 
 struct WaveX <: DelayComponent end
 
+fourier_term(Φ, a, b) = dot((a, b), sincos(Φ))
+
 function delay(::WaveX, ctoa::CorrectedTOA, params::NamedTuple)::GQ
     t_t0 = corrected_toa_value(ctoa) - params.WXEPOCH
     as, bs = params.WXSIN_, params.WXCOS_
     fs = params.WXFREQ_
     k = 2 * π * t_t0
     Φs = map(f -> k * f, fs)
-    fourier_term = (Φ, a, b) -> dot((a, b), sincos(Φ))
     return mapreduce(fourier_term, +, Φs, as, bs)
 end
 
@@ -20,6 +21,5 @@ function dispersion_slope(::DMWaveX, ctoa::CorrectedTOA, params::NamedTuple)::GQ
     fs = params.DMWXFREQ_
     k = 2 * π * t_t0
     Φs = map(f -> k * f, fs)
-    fourier_term = (Φ, a, b) -> dot((a, b), sincos(Φ))
     return mapreduce(fourier_term, +, Φs, as, bs)
 end
