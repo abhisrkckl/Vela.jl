215e367b141981808946ed491ff79a2e1d587617
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-26 14:47:31 +0200

    simple_prior

--- src/prior/prior.jl
+++ src/prior/prior.jl
@@ -1,13 +1,7 @@
-export Prior,
-    SimplePrior, lnprior, prior_transform, get_lnprior_func, get_prior_transform_func, distr
+export Prior, lnprior, prior_transform, get_lnprior_func, get_prior_transform_func, distr
 
 """Abstract base class of all priors."""
 abstract type Prior end
-
-"""Extract the value of a parameter for evaluating its prior."""
-param_value(prior::Prior, params::NamedTuple) = param_value(prior, params[prior.name])
-param_value(::Prior, param::GQ) = value(param)
-param_value(prior::Prior, params::NTuple) = value(params[prior.index])
 
 """Evaluate the log-prior."""
 lnprior(prior::Prior, params::NamedTuple) =
@@ -20,15 +14,3 @@ get_lnprior_func(model::TimingModel) = params -> lnprior(model, params)
 prior_transform(priors, cube) = map(prior_transform, priors, cube)
 prior_transform(model::TimingModel, cube) = prior_transform(model.priors, cube)
 get_prior_transform_func(model::TimingModel) = cube -> prior_transform(model, cube)
-
-struct SimplePrior{D<:Distribution} <: Prior
-    name::Symbol
-    index::UInt
-    distribution::D
-end
-
-SimplePrior(name::Symbol, distr::Distribution) = SimplePrior(name, UInt(0), distr)
-
-distr(sp::SimplePrior, ::NamedTuple)::Distribution = sp.distribution
-
-prior_transform(prior::SimplePrior, q) = quantile(prior.distribution, q)
---
+++ src/prior/simple_prior.jl
@@ -0,0 +1,36 @@
+export SimplePrior, SimplePriorMulti
+
+abstract type SimplePriorBase <: Prior end
+
+distr(sp::SimplePriorBase, ::NamedTuple)::Distribution = sp.distribution
+prior_transform(prior::SimplePriorBase, q) = quantile(prior.distribution, q)
+
+struct SimplePrior{name,D<:Distribution} <: SimplePriorBase
+    distribution::D
+end
+
+function SimplePrior{name}(distr::Distribution) where {name}
+    @assert name isa Symbol
+    return SimplePrior{name,typeof(distr)}(distr)
+end
+
+param_name(::SimplePrior{name,D}) where {name,D<:Distribution} = name
+param_value(::SimplePrior{name,D}, params::NamedTuple) where {name,D<:Distribution} =
+    value(params[name])
+
+struct SimplePriorMulti{name,index,D<:Distribution} <: SimplePriorBase
+    distribution::D
+end
+
+function SimplePriorMulti{name,index}(distr::Distribution) where {name,index}
+    @assert name isa Symbol
+    @assert index isa UInt
+    return SimplePriorMulti{name,index,typeof(distr)}(distr)
+end
+
+param_name(::SimplePriorMulti{name,index,D}) where {name,index,D<:Distribution} = name
+param_index(::SimplePriorMulti{name,index,D}) where {name,index,D<:Distribution} = index
+param_value(
+    ::SimplePriorMulti{name,index,D},
+    params::NamedTuple,
+) where {name,index,D<:Distribution} = value(params[name][index])
