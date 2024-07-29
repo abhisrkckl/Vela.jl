6af217837c467c171c5559fddd1f783309a2553e
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-29 16:11:11 +0200

    evaluate_xwavex

--- src/model/wavex.jl
+++ src/model/wavex.jl
@@ -4,22 +4,22 @@ struct WaveX <: DelayComponent end
 
 fourier_term(Φ, a, b) = dot((a, b), sincos(Φ))
 
-function delay(::WaveX, ctoa::CorrectedTOA, params::NamedTuple)::GQ
-    t_t0 = corrected_toa_value(ctoa) - params.WXEPOCH
-    as, bs = params.WXSIN_, params.WXCOS_
-    fs = params.WXFREQ_
+function evaluate_xwavex(ctoa::CorrectedTOA, epoch::GQ, as::NTuple, bs::NTuple, fs::NTuple)
+    t_t0 = corrected_toa_value(ctoa) - epoch
     k = 2 * π * t_t0
     Φs = map(f -> k * f, fs)
     return mapreduce(fourier_term, +, Φs, as, bs)
 end
+
+delay(::WaveX, ctoa::CorrectedTOA, params::NamedTuple)::GQ =
+    evaluate_xwavex(ctoa, params.WXEPOCH, params.WXSIN_, params.WXCOS_, params.WXFREQ_)
 
 struct DMWaveX <: DispersionComponent end
 
-function dispersion_slope(::DMWaveX, ctoa::CorrectedTOA, params::NamedTuple)::GQ
-    t_t0 = corrected_toa_value(ctoa) - params.DMWXEPOCH
-    as, bs = params.DMWXSIN_, params.DMWXCOS_
-    fs = params.DMWXFREQ_
-    k = 2 * π * t_t0
-    Φs = map(f -> k * f, fs)
-    return mapreduce(fourier_term, +, Φs, as, bs)
-end
+dispersion_slope(::DMWaveX, ctoa::CorrectedTOA, params::NamedTuple)::GQ = evaluate_xwavex(
+    ctoa,
+    params.DMWXEPOCH,
+    params.DMWXSIN_,
+    params.DMWXCOS_,
+    params.DMWXFREQ_,
+)
