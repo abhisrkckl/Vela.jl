b502324f8808ff3ea8140636c1aea3c789dcff27
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-07-30 13:20:32 +0200

    test_NGC6440E

--- test/test_NGC6440E.jl
+++ test/test_NGC6440E.jl
@@ -60,6 +60,13 @@
             ),
         ) == length(param_handler._free_indices)
 
+        @test length(
+            read_param_values_to_vector(
+                model,
+                model.param_handler._default_params_tuple,
+            ),
+        ) == length(param_handler._free_indices)
+
         @test all(isfinite.(get_scale_factors(model))) &&
               !any(iszero.(get_scale_factors(model)))
     end
