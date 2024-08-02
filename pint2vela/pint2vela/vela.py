0136ebe27f4eb43ced0f2c901ef56d51fa1b399a
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-08-02 13:23:19 +0200

    --

--- pint2vela/pint2vela/vela.py
+++ pint2vela/pint2vela/vela.py
@@ -1,5 +1,13 @@
 from juliacall import Main as jl
+import numpy as np
 
 jl.seval("import Vela")
 jl.seval("using Distributions")
+jl.seval("using DoubleFloats")
 vl = jl.Vela
+
+
+def to_jldd(x_: np.longdouble):
+    x = float(x_)
+    y = float(x_ - x)
+    return jl.Double64(x, y)
