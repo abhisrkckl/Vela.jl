49ddcdc014579c8a027baf598ce15e75605ee221
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-08-22 15:38:55 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="pint2vela",
-    version="0.0.2",
+    version="0.0.3",
     description="Interface between PINT and Vela.jl",
     author="Abhimanyu Susobhanan",
     author_email="abhimanyu.susobhanan@nanograv.org",
