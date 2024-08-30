208d31b427c089d1b2ddfded10c56543dd005672
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-08-30 15:42:02 +0200

    fix test_priors

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,7 +30,7 @@ const obs_earth_pos = distance.((0.01199435, 0.01159591, -0.01316261))
 
     include("test_components.jl")
 
-    # include("test_priors.jl")
+    include("test_priors.jl")
 
     # include("test_pure_rotator.jl")
 
