c1ef5d2146be9266a44d2a79ae28460ea06eae90
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-09-04 15:18:46 +0200

    wideband toa code

---
+++ src/toa/wideband_toa.jl
@@ -0,0 +1,43 @@
+"""DM information associated with a wideband TOA."""
+struct DMInfo
+    value::GQ{-1,Float64}
+    error::GQ{-1,Float64}
+end
+
+"""The accumulated timing & noise model corrections applied to wideband DM."""
+struct CorrectedDMInfo
+    dminfo::DMInfo
+    model_dm::GQ{-1,Float64}
+    dmefac::GQ{0,Float64}
+    dmequad2::GQ{-2,Float64}
+end
+
+CorrectedDMInfo(dminfo::DMInfo) =
+    CorrectedDMInfo(dminfo, GQ{-1}(0.0), dimensionless(1.0), GQ{-2}(0.0))
+
+correct_dminfo(
+    cdminfo::CorrectedDMInfo;
+    model_dm = GQ{-1}(0.0),
+    dmefac = dimensionless(1.0),
+    dmequad2 = GQ{-2}(0.0),
+) = CorrectedDMInfo(
+    cdminfo.dminfo,
+    cdminfo.model_dm + model_dm,
+    cdminfo.dmefac * dmefac,
+    cdminfo.dmequad2 + dmequad2,
+)
+
+"""A single wideband TOA observation."""
+struct WidebandTOA
+    toa::TOA
+    dminfo::DMInfo
+end
+
+"""The accumulated timing & noise model corrections applied to a wideband TOA."""
+struct CorrectedWidebandTOA
+    corrected_toa::CorrectedTOA
+    corrected_dminfo::CorrectedDMInfo
+end
+
+CorrectedWidebandTOA(wtoa::WidebandTOA) =
+    CorrectedWidebandTOA(CorrectedTOA(wtoa.toa), CorrectedDMInfo(wtoa.dminfo))
