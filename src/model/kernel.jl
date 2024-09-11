de807c8bb9d3b775daf7e77303e2d0109eba18f7
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-09-11 09:24:05 +0200

    kernel

---
+++ src/model/kernel.jl
@@ -0,0 +1,10 @@
+"""Abstract base class of all likelihood kernels"""
+abstract type Kernel end
+
+"""A kernel representing only uncorrelated noise.
+The covariance matrix is diagonal."""
+struct WhiteNoiseKernel <: Kernel end
+
+"""A kernel representing only time-uncorrelated noise.
+The covariance matrix is block-diagonal."""
+struct EcorrKernel <: Kernel end
