650774f5a2c94a87ef6e4d4245fb5fd12d27b6c2
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-09-11 15:40:25 +0200

    ecorr_chi2

--- src/model/kernel.jl
+++ src/model/kernel.jl
@@ -26,5 +26,5 @@ end
 show(io::IO, ::MIME"text/plain", model::Kernel) = show(io, model)
 show(io::IO, ek::EcorrKernel) = print(
     io,
-    "EcorrKernel[$(length(unique(grp.index for grp in ek.ecorr_groups)) - 1) ECORRs, $(length(ek.ecorr_groups)) groups]",
+    "EcorrKernel($(length(unique(grp.index for grp in ek.ecorr_groups)) - 1) ECORRs, $(length(ek.ecorr_groups)) groups)",
 )
