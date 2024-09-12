56ab441cd81a3062c8fa2a0e516d1dd4bce6e8bd
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-09-12 11:34:40 +0200

    test error

--- pint2vela/tests/test_ecorr.py
+++ pint2vela/tests/test_ecorr.py
@@ -1,9 +1,12 @@
-from pint2vela.ecorr import ecorr_sort
-from pint2vela import read_model_and_toas
-from pint.models import get_model_and_toas
+from io import StringIO
 
 import numpy as np
 import pytest
+from pint.models import get_model, get_model_and_toas
+
+from pint2vela import read_model_and_toas
+from pint2vela.ecorr import ecorr_sort
+from pint2vela.pint2vela import get_kernel
 
 
 @pytest.fixture(scope="module")
@@ -43,3 +46,45 @@ def test_read_data(data_B1855p09):
     assert len(t_sorted) == len(tv)
     assert len(mv.kernel.ecorr_groups) == len(indices)
     assert mv.kernel.ecorr_groups[0].index == 0
+
+
+def test_plrednoise_error():
+    par = """
+        PSR                                  SIM2
+        EPHEM                               DE440
+        CLOCK                        TT(BIPM2019)
+        UNITS                                 TDB
+        START              53000.9999999566961806
+        FINISH             56985.0000000464502315
+        DILATEFREQ                              N
+        DMDATA                                  N
+        NTOA                                 2000
+        CHI2                   1999.9595338710205
+        CHI2R                  1.0039957499352512
+        TRES                1.5197086404438400383
+        RAJ                      5:00:00.00000104 1 0.00000309788366042071
+        DECJ                    14:59:59.99982230 1 0.00026526190479642293
+        PMRA                                  0.0
+        PMDEC                                 0.0
+        PX                                    0.0
+        F0                  100.00000000000000391 1 6.1147023828669208493e-14
+        F1              -9.999985179158219319e-16 1 1.3675433277483732467e-21
+        F2                                    0.0
+        PEPOCH             55000.0000000000000000
+        EFAC            tel gbt        1.2663639225043157 1 0.021424340920438052
+        ECORR           tel gbt        0.8409712702910475 1 0.049670237117127584
+        PLANET_SHAPIRO                          N
+        DM                                   15.0
+        DM1                                   0.0
+        TZRMJD             55000.0000000000000000
+        TZRSITE                               gbt
+        TZRFRQ                             1400.0
+        PHOFF              4.2709307823244897e-07 1 9.1199685066235e-06
+        TNREDAMP                              -14
+        TNREDGAM                              3.5
+        TNREDC                                 15
+    """
+    m = get_model(StringIO(par))
+
+    with pytest.raises(NotImplementedError):
+        get_kernel(m)
