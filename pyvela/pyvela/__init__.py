5bef2727a56e6ba2e02fd79cc1785fb8629ef1f6
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-04-29 09:56:38 +0200

    check version

--- pyvela/pyvela/__init__.py
+++ pyvela/pyvela/__init__.py
@@ -6,3 +6,7 @@ from .spnta import SPNTA
 from .vela import vl as Vela
 
 __version__ = metadata.version("pyvela")
+
+assert (
+    __version__ == Vela.pkg_version()
+), f"Version mismatch between Vela and pyvela! Please check your installation. Vela version is {Vela.pkg_version()} and pyvela version is {__version__}."
