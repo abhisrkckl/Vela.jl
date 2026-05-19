f688144d1f6d3892f1bf20ed7be2a9f3e2790500
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-05-19 12:47:24 +0530

    fix tests

--- test/test_sim_dmgp_wb.jl
+++ test/test_sim_dmgp_wb.jl
@@ -90,11 +90,11 @@
         @test length(model.kernel.gp_components) == 2
     end
 
-    @testset "_calc_resids_and_Ndiag" begin
+    @testset "_calc_resids_and_Ninvdiag" begin
         params = model.param_handler._default_params_tuple
-        y, Ndiag = Vela._calc_resids_and_Ndiag(model, toas, params)
-        @test length(y) == length(Ndiag) == 2*length(toas)
-        @test all(Ndiag .> 0)
+        y, Ninvdiag = Vela._calc_resids_and_Ninvdiag(model, toas, params)
+        @test length(y) == length(Ninvdiag) == 2*length(toas)
+        @test all(Ninvdiag .> 0)
     end
 
     @testset "calc_lnlike" begin
