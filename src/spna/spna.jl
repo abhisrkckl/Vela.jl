075e2ebe0f0b9d44461a648509c9f252535446f4
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-07-15 10:21:40 +0530

    make_SPNA

--- src/spna/spna.jl
+++ src/spna/spna.jl
@@ -1,3 +1,5 @@
+export make_SPNA
+
 abstract type SPNABase end
 
 struct NarrowbandSPNA{
@@ -38,6 +40,11 @@ WidebandSPNA(model::TimingModel, wtoas::Vector{WidebandTOA}, Mtm::Array{Float64,
         make_spna_param_handler(model),
         model.priors[(get_num_timing_params(model)+1):end],
     )
+
+make_SPNA(model::TimingModel, toas::Vector{TOA}, Mtm::Array{Float64,2}) =
+    NarrowbandSPNA(model, toas, Mtm)
+make_SPNA(model::TimingModel, wtoas::Vector{WidebandTOA}, Mtm::Array{Float64,2}) =
+    WidebandSPNA(model, wtoas, Mtm)
 
 show(io::IO, spna::NarrowbandSPNA) =
     print(io, "NarrowbandSPNA[$(size(spna.kernel.noise_basis))]")
