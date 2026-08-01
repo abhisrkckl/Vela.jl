95bc2b1f02bf1bf11b5f482ebc5256cbd02d6e4f
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-08-01 09:04:20 +0530

    threads

--- pyvela/pyvela/pyvela_poco_script.py
+++ pyvela/pyvela/pyvela_poco_script.py
@@ -247,7 +247,7 @@ def main(argv=None):
         likelihood=spnta.lnlike_vectorized,
         vectorize=True,
         output_dir=args.outdir,
-        # pytorch_threads=int(os.environ["PYTHON_JULIACALL_THREADS"]),
+        pytorch_threads=int(os.environ["PYTHON_JULIACALL_THREADS"]),
     )
     sampler.run(
         n_total=args.nsamples,
