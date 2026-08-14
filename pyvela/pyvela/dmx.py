052e7b6795c8231d9b7338cd08ea5212f29e58be
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-08-14 11:08:54 +0530

    fix get_dmx_mask

--- pyvela/pyvela/dmx.py
+++ pyvela/pyvela/dmx.py
@@ -1,10 +1,7 @@
-from typing import List
-
 import numpy as np
-from astropy.time import Time
 from pint.models import TimingModel
 from pint.toa import TOAs
-from pint.utils import find_prefix_bytime
+from pint.utils import get_prefix_timeranges
 
 
 def get_dmx_mask(
@@ -13,22 +10,17 @@ def get_dmx_mask(
     """Get a Vela-compatible DMX/CMX mask given a timing model.
     The output is an ndarray containing the DMX/CMX index for each TOA.
     Throws an exception if overlapping DMX ranges are found."""
-    mask: List[int] = []
-    for ii in range(len(toas)):
-        t: Time = toas.table["mjd"][ii]
-        idxs = find_prefix_bytime(model, param_prefix, t)
-        if np.isscalar(idxs):
-            mask.append(int(idxs))
-        elif len(idxs) == 0:
-            # mask.append(0)
-            raise ValueError(
-                f"TOA number {ii} ({str(t)}) does not belong to a {param_prefix} range."
-            )
-        else:
-            raise ValueError(
-                f"Multiple {param_prefix} ranges found for TOA number {ii} ({str(t)})."
-            )
-    assert len(mask) == len(
-        toas
-    ), "Length of the DMX mask is inconsistent with the number of TOAs. This is a bug."
-    return np.array(mask, dtype=int)
+
+    indices, r1s, r2s = get_prefix_timeranges(model, param_prefix)
+    r1s = r1s.mjd
+    r2s = r2s.mjd
+
+    output_mask = np.repeat(0, len(toas))
+    mjds = toas.get_mjds().value
+    for ii, r1, r2 in zip(indices, r1s, r2s):
+        bool_mask = np.logical_and(mjds >= r1, mjds < r2)
+        assert np.any(bool_mask), f"No TOAs found for {param_prefix}{ii:4d}."
+        output_mask[bool_mask] = ii
+    assert np.all(output_mask > 0), f"Some TOAs don't belong to any {param_prefix} ranges."
+    
+    return output_mask
