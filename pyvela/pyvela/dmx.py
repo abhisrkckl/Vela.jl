994e623ead2d0616dfb32a00a9d2bbc55e7114bd
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-08-14 11:44:08 +0530

    fix tests

--- pyvela/pyvela/dmx.py
+++ pyvela/pyvela/dmx.py
@@ -15,6 +15,8 @@ def get_dmx_mask(
     r1s = r1s.mjd
     r2s = r2s.mjd
 
+    assert all(r1s[1:] > r2s[:-1]), f"Overlapping {param_prefix} ranges found!"
+
     output_mask = np.repeat(0, len(toas))
     mjds = toas.get_mjds().value
     for ii, r1, r2 in zip(indices, r1s, r2s):
