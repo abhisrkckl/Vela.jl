91ca47e4f43f1261a5c5c9c3cda64c443a4cc832
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-08-26 13:46:09 +0530

    restore

--- pyvela/pyvela/pyvela_script.py
+++ pyvela/pyvela/pyvela_script.py
@@ -4,7 +4,6 @@ import os
 import shutil
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
-import numpy as np
 import emcee
 
 from pint.logging import setup as setup_log
@@ -285,15 +284,14 @@ def main(argv=None):
     sampler = emcee.EnsembleSampler(
         nwalkers,
         spnta.ndim,
-        spnta.lnpost_transformed_vectorized,
+        spnta.lnpost_vectorized,
         moves=[emcee.moves.StretchMove(), emcee.moves.DESnookerMove()],
         vectorize=True,
         backend=emcee.backends.HDFBackend(f"{args.outdir}/chain.h5"),
     )
 
     p0 = (
-        # get_start_samples(spnta, args.initial_sample_spread, nwalkers)
-        (np.random.rand(nwalkers, spnta.ndim) - 0.5) * args.initial_sample_spread + 0.5
+        get_start_samples(spnta, args.initial_sample_spread, nwalkers)
         if not args.resume
         else sampler.get_chain()[-1, :, :]
     )
@@ -315,11 +313,9 @@ def main(argv=None):
     #     p0, args.nsteps, progress=True, progress_kwargs={"mininterval": 1}
     # )
 
-    samples_transformed = sampler.get_chain(
-        flat=True, discard=min(args.burnin, int(25 * max(tau))), thin=args.thin
+    samples_raw = sampler.get_chain(
+        flat=True, discard=min(args.burnin, int(10 * max(tau))), thin=args.thin
     )
-
-    samples_raw = spnta.prior_transform(samples_transformed)
 
     spnta.save_results(
         args.outdir,
