e8ea81ae0fe9fbebcb4a32c80f84b2bf493fe119
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-08-26 17:28:14 +0530

    fix indices

--- src/likelihood/parallel.jl
+++ src/likelihood/parallel.jl
@@ -2,12 +2,7 @@ function calc_noise_weights_inv(
     kernel::WoodburyKernel,
     params_list::AbstractVector{<:NamedTuple},
 )
-    return tuple(
-        (
-            calc_noise_weights_inv(gp_comp, params) for params in params_list for
-            gp_comp in kernel.gp_components
-        )...,
-    )
+    return map(params -> calc_noise_weights_inv(kernel, params), params_list)
 end
 
 function calc_resids_and_Ninvdiag(
@@ -18,19 +13,17 @@ function calc_resids_and_Ninvdiag(
     nsets = length(params_list)
     ntoas = length(toas)
 
-    result_data = Vector{Float64}(undef, 2 * nsets * ntoas)
-    ys = @view result_data[1:(nsets*ntoas)]
-    Ninvdiag = @view result_data[(nsets*ntoas+1):end]
+    result_data = Array{Float64}(undef, ntoas, 2, nsets)
 
-    Threads.@threads for i = 1:nsets
+    Threads.@threads for i in eachindex(params_list)
         params = params_list[i]
         tzrphase = calc_tzr_phase(model, params)
 
-        inds = ((i-1)*ntoas+1):(i*ntoas)
-        ys_view = @view ys[inds]
-        Ninvdiag_view = @view Ninvdiag[inds]
+        ys_view = @view result_data[:, 1, i]
+        Ninvdiag_view = @view result_data[:, 2, i]
 
-        @inbounds for (j, toa) in enumerate(toas)
+        @inbounds for j in eachindex(toas)
+            toa = toas[j]
             ctoa = correct_toa(model, toa, params)
             dphase = GQ{Float64}(phase_residual(toa, ctoa) - tzrphase)
 
@@ -38,6 +31,9 @@ function calc_resids_and_Ninvdiag(
             Ninvdiag_view[j] = 1.0 / value(scaled_toa_error_sqr(toa, ctoa))
         end
     end
+
+    ys = @view result_data[:, 1, :]
+    Ninvdiag = @view result_data[:, 2, :]
 
     return ys, Ninvdiag
 end
@@ -50,30 +46,32 @@ function calc_resids_and_Ninvdiag(
     nsets = length(params_list)
     ntoas = length(wtoas)
 
-    result_data = Vector{Float64}(undef, 4 * nsets * ntoas)
-    ys = @view result_data[1:(2*nsets*ntoas)]
-    Ninvdiag = @view result_data[(2*nsets*ntoas+1):end]
+    result_data = Array{Float64}(undef, 2*ntoas, 2, nsets)
 
-    Threads.@threads for i = 1:nsets
+    Threads.@threads for i in eachindex(params_list)
         params = params_list[i]
         tzrphase = calc_tzr_phase(model, params)
 
-        inds = ((i-1)*2*ntoas+1):(i*2*ntoas)
-        ys_view = @view ys[inds]
-        Ninvdiag_view = @view Ninvdiag[inds]
+        ys_view = @view result_data[:, 1, i]
+        Ninvdiag_view = @view result_data[:, 2, i]
 
         @inbounds for (j, wtoa) in enumerate(wtoas)
             cwtoa = correct_toa(model, wtoa, params)
             dphase = GQ{Float64}(phase_residual(wtoa.toa, cwtoa.toa_correction) - tzrphase)
 
-            ys_view[j] = dphase / doppler_shifted_spin_frequency(cwtoa.toa_correction)
-            ys_view[ntoas+j] = dm_residual(wtoa.dminfo, cwtoa.dm_correction)
+            ys_view[j] =
+                value(dphase / doppler_shifted_spin_frequency(cwtoa.toa_correction))
+            ys_view[ntoas+j] = value(dm_residual(wtoa.dminfo, cwtoa.dm_correction))
 
-            Ninvdiag_view[j] = 1.0 / scaled_toa_error_sqr(wtoa.toa, cwtoa.toa_correction)
+            Ninvdiag_view[j] =
+                1.0 / value(scaled_toa_error_sqr(wtoa.toa, cwtoa.toa_correction))
             Ninvdiag_view[ntoas+j] =
-                1.0 / scaled_dm_error_sqr(wtoa.dminfo, cwtoa.dm_correction)
+                1.0 / value(scaled_dm_error_sqr(wtoa.dminfo, cwtoa.dm_correction))
         end
     end
+
+    ys = @view result_data[:, 1, :]
+    Ninvdiag = @view result_data[:, 2, :]
 
     return ys, Ninvdiag
 end
