8529700ff08789251a7de958670d1d1f1403d392
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-09-01 09:31:20 +0530

    --

--- src/likelihood/parallel.jl
+++ src/likelihood/parallel.jl
@@ -85,7 +85,7 @@ function _calc_y_Ninv_y__and__logdet_N(
     @assert size(Ninvdiag) == size(y)
     Ndata, Nsets = size(y)
 
-    result_data = zeros(Float64, (2, Nsets))
+    result_data = Array{Float64}(undef, 2, Nsets)
 
     y_Ninv_y = @view result_data[1, :]
     logdet_N = @view result_data[2, :]
@@ -102,4 +102,83 @@ function _calc_y_Ninv_y__and__logdet_N(
     end
 
     return y_Ninv_y, logdet_N
+end
+
+function _calc_Ninv_M(
+    ::WhiteNoiseKernel,
+    M::AbstractMatrix,
+    Ninvdiag::AbstractMatrix,
+    ::AbstractVector{<:NamedTuple};
+)
+    Ntoa, Npar = size(M)
+    Nsets = size(Ninvdiag)[2]
+    @assert size(Ninvdiag)[1] == Ntoa
+
+    X = eltype(M)
+
+    Ninv_M = Array{X}(undef, Ntoa, Npar, Nsets)
+    Threads.@threads for i = 1:Nsets
+        for p = 1:Npar
+            @inbounds @simd for j = 1:Ntoa
+                Ninv_M[j, p, i] = M[j, p] * Ninvdiag[j, i]
+            end # COV_EXCL_LINE
+        end
+    end
+
+    return Ninv_M
+end
+
+
+
+
+
+
+
+
+
+
+
+
+function _calc_Σinv__and__MT_Ninv_y(
+    inner_kernel::Kernel,
+    M::AbstractMatrix,
+    Ninvdiag::AbstractMatrix,
+    Φinv::AbstractVector,
+    y::AbstractVector,
+    params::NamedTuple,
+)
+    Ntoa, Npar = size(M)
+    @assert length(Ninvdiag) == length(y) == Ntoa
+    @assert length(Φinv) == Npar
+
+    Ninv_M = _calc_Ninv_M(inner_kernel, M, Ninvdiag, params)
+
+    X = eltype(M)
+
+    # TODO: Only allocate memory for lower triangular elements.
+    result_data = Matrix{X}(undef, Npar, Npar + 1)
+    Σinv = @view result_data[:, 1:Npar]
+    @inbounds @threads for p = 1:Npar
+        for q = 1:p
+            Σinv_qp = (p == q) ? Φinv[p] : zero(Φinv[p])
+            @simd for j = 1:Ntoa
+                Σinv_qp += M[j, p] * Ninv_M[j, q]
+            end # COV_EXCL_LINE
+
+            # Only upper triangular elements are populated.
+            # The rest contain garbage.
+            Σinv[q, p] = Σinv_qp
+        end
+    end # COV_EXCL_LINE
+
+    u = @view result_data[:, Npar+1]
+    @inbounds @threads for p = 1:Npar
+        up = zero(X)
+        @simd for j = 1:Ntoa
+            up += Ninv_M[j, p] * y[j]
+        end # COV_EXCL_LINE
+        u[p] = up
+    end # COV_EXCL_LINE
+
+    return Symmetric(Σinv, :U), u
 end
