c8eaae1734585ed0f1c06675aa35eec42af0f249
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-09-07 21:35:03 +0530

    gls_likelihood

--- src/likelihood/gls_likelihood.jl
+++ src/likelihood/gls_likelihood.jl
@@ -2,7 +2,10 @@ export calc_lnlike_vectorized
 
 function calc_noise_weights_inv(kernel::WoodburyKernel, params::NamedTuple)
     return vcat(
-        (calc_noise_weights_inv(gp_comp, params) for gp_comp in kernel.gp_components)...,
+        (
+            Float32.(calc_noise_weights_inv(gp_comp, params)) for
+            gp_comp in kernel.gp_components
+        )...,
     )
 end
 
@@ -16,7 +19,7 @@ function _calc_resids_and_Ninvdiag(
 
     ntoas = length(toas)
 
-    result_data = Vector{Float64}(undef, 2 * ntoas)
+    result_data = Vector{Float32}(undef, 2 * ntoas)
     ys = @view result_data[1:ntoas]
     Ninvdiag = @view result_data[(ntoas+1):end]
 
@@ -50,7 +53,7 @@ function _calc_resids_and_Ninvdiag(
     tzrphase = calc_tzr_phase(model, params)
 
     ntoas = length(wtoas)
-    result_data = Vector{Float64}(undef, 4 * ntoas)
+    result_data = Vector{Float32}(undef, 4 * ntoas)
     ys = @view result_data[1:(2*ntoas)]
     Ninvdiag = @view result_data[(2*ntoas+1):end]
 
@@ -59,19 +62,21 @@ function _calc_resids_and_Ninvdiag(
             wtoa = wtoas[j]
             cwtoa = correct_toa(model, wtoa, params)
             dphase = GQ{Float64}(phase_residual(wtoa.toa, cwtoa.toa_correction) - tzrphase)
-            ys[j] = dphase / doppler_shifted_spin_frequency(cwtoa.toa_correction)
-            ys[ntoas+j] = dm_residual(wtoa.dminfo, cwtoa.dm_correction)
-            Ninvdiag[j] = 1.0 / scaled_toa_error_sqr(wtoa.toa, cwtoa.toa_correction)
-            Ninvdiag[ntoas+j] = 1.0 / scaled_dm_error_sqr(wtoa.dminfo, cwtoa.dm_correction)
+            ys[j] = value(dphase / doppler_shifted_spin_frequency(cwtoa.toa_correction))
+            ys[ntoas+j] = value(dm_residual(wtoa.dminfo, cwtoa.dm_correction))
+            Ninvdiag[j] = value(1.0 / scaled_toa_error_sqr(wtoa.toa, cwtoa.toa_correction))
+            Ninvdiag[ntoas+j] =
+                value(1.0 / scaled_dm_error_sqr(wtoa.dminfo, cwtoa.dm_correction))
         end # COV_EXCL_LINE
     else
         @inbounds for (j, wtoa) in enumerate(wtoas)
             cwtoa = correct_toa(model, wtoa, params)
             dphase = GQ{Float64}(phase_residual(wtoa.toa, cwtoa.toa_correction) - tzrphase)
-            ys[j] = dphase / doppler_shifted_spin_frequency(cwtoa.toa_correction)
-            ys[ntoas+j] = dm_residual(wtoa.dminfo, cwtoa.dm_correction)
-            Ninvdiag[j] = 1.0 / scaled_toa_error_sqr(wtoa.toa, cwtoa.toa_correction)
-            Ninvdiag[ntoas+j] = 1.0 / scaled_dm_error_sqr(wtoa.dminfo, cwtoa.dm_correction)
+            ys[j] = value(dphase / doppler_shifted_spin_frequency(cwtoa.toa_correction))
+            ys[ntoas+j] = value(dm_residual(wtoa.dminfo, cwtoa.dm_correction))
+            Ninvdiag[j] = value(1.0 / scaled_toa_error_sqr(wtoa.toa, cwtoa.toa_correction))
+            Ninvdiag[ntoas+j] =
+                value(1.0 / scaled_dm_error_sqr(wtoa.dminfo, cwtoa.dm_correction))
         end
     end
 
@@ -301,7 +306,7 @@ function calc_lnlike_vectorized(
     paramss,
 ) where {T<:TOABase}
     nparamss = size(paramss)[1]
-    result = Vector{Float64}(undef, nparamss)
+    result = Vector{Float32}(undef, nparamss)
     @threads :static for ii = 1:nparamss
         result[ii] = calc_lnlike_serial(model, toas, paramss[ii, :])
     end # COV_EXCL_LINE
