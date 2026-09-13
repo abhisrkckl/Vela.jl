261ee40b0d7f5101b6566e5890bb0ea0c311392c
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2026-09-13 11:40:35 +0530

    load_pulsar_data

--- src/readwrite/readwrite.jl
+++ src/readwrite/readwrite.jl
@@ -8,5 +8,30 @@ end
 
 function load_pulsar_data(filename::String)
     data = JLSO.load(filename)
-    return data[:model], data[:toas]
+    model, toas = data[:model], data[:toas]
+
+    if isa(model.kernel, WoodburyKernel)
+        # The WoodburyKernel must be re-made because the kernel workspace size
+        # depends on the number of available threads.
+        kernel = WoodburyKernel(
+            model.kernel.inner_kernel,
+            model.kernel.gp_components,
+            model.kernel.noise_basis,
+        )
+        return TimingModel(
+            model.pulsar_name,
+            model.ephem,
+            model.clock,
+            model.units,
+            model.epoch,
+            model.components,
+            kernel,
+            model.param_handler,
+            model.tzr_toa,
+            model.priors,
+        ),
+        toas
+    else
+        return model, toas
+    end
 end
